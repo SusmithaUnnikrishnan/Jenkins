c7382771339323baee8d3f4233730d17d6ac8675
SUSMITHA <SUSMITHA@LAPTOP-6C34LHA8>
2024-03-22 12:49:03 +0530

    commit

---
+++ JenkinsMaven/src/main/java/sample/SampleProgram.java
@@ -0,0 +1,18 @@
+package sample;
+
+public class SampleProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			int age=12;
+			if (age>=18){
+				System.out.println("You are eligible to vote");
+			}
+				else {
+					System.out.println("You are not eligible");
+				}
+
+	}
+
+}
